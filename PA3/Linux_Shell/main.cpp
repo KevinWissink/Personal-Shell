de53fa6184aef755f0867b6f0b176e9d45a0a841
Kevin <kevinwissink@tamu.edu>
2020-03-19 20:32:42 -0500

    Got the input redirection working

--- PA3/Linux_Shell/main.cpp
+++ PA3/Linux_Shell/main.cpp
@@ -2,9 +2,13 @@
 #include <sstream>
 #include <iterator>
 #include <vector>
+#include <algorithm>
+#include <fcntl.h>
 #include <iostream>
 #include <unistd.h>
 #include <sys/wait.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 
 using namespace std;
 
@@ -29,6 +33,23 @@ int main(){
         istream_iterator<string> end;
         vector<string> elements(begin, end);
 
+        //checking if the input need to be I/O redirection
+        bool out, in;
+        for(int i = 0; i < elements.size(); i++)
+        {
+            
+            if(elements[i] == ">")
+            {
+                in = true;
+                cout << "Input Redirection" << endl;
+            }
+            if(elements[i] == "<")
+            {
+                out = true;
+                cout << "Output Redirection" << endl;
+            }
+        }
+
         int pid = fork();
         if (pid == -1)
         {
@@ -36,27 +57,84 @@ int main(){
         }
         else if (pid == 0 )
         {
-            int size = elements.size();
-            char* args[size+1]; 
-            for (int i = 0; i < size; ++i) {
-                args[i] = (char*) elements[i].c_str();
-            }
+            //dealing with basic commands
+            if(!in && !out)
+            {
+                int size = elements.size();
+                char* args[size+1]; 
+                for (int i = 0; i < size; ++i) {
+                    args[i] = (char*) elements[i].c_str();
+                }
 
-            args[size] = NULL;
+                args[size] = NULL;
 
-            //testing purposes
-            //for (char* cs : args) {
-            //     cout << cs << " ";
-            ///}
+                //testing purposes
+                //for (char* cs : args) {
+                //     cout << cs << " ";
+                //}
             
 
-            if(execvp (args[0], args) < 0) {
-                printf("Could not execute command..\n");
+                if(execvp (args[0], args) < 0) {
+                    printf("Could not execute command..\n");
+                }
             }
+
+            if (in & !out){
+                int size = elements.size();
+                string file = elements[size-1] + ".txt";
+                const char* filename = file.c_str();
+
+                cout << "Filename: " << filename << endl;
+                
+                int iter = 0;
+
+                vector<string>::iterator it = find(elements.begin(), elements.end(), ">");
+                int index = distance(elements.begin(), it);
+
+                cout << "Indicator at: " << index << endl;
+
+                char* args[index + 1];
+                args[index] = NULL;
+
+                while(index != -1)
+                {
+                    index--;
+                    args[index] = (char*) elements[index].c_str();
+                }
+                
+                //making sure the file opens
+                int fd;
+                if ((fd = open(filename, O_WRONLY, 0)) < 0){
+                    cout << "Error in opening file" <<  endl;
+                    break;
+                }    
+
+                //redirect the standard out to the file
+                dup2(fd, 1);
+
+                close(fd);
+
+
+                if(execvp(args[0], args) < 0)
+                {
+                    printf("Could not execute command.. \n");
+                }
+            }
+
+            if (!in & out){
+                int size = elements.size();
+                int fd;
+                string filename = elements[size-1];
+
+                
+            }
+
         }
         else 
         {
             wait(0);
+            in = false;
+            out = false;
         }
     }
 }
